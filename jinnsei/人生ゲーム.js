5d858667dc759058ae7d10d283dce6e200cdd38f
Takkun3 <56062548+Takkun3@users.noreply.github.com>
2020-04-23 22:50:51 +0900

    奇数ます偶数ますで進んだり戻ったり

--- jinnsei/人生ゲーム.js
+++ jinnsei/人生ゲーム.js
@@ -3,8 +3,21 @@ $(function(){
     var saiko;
     var cnt=0;
     var left=0;
-    var topp
+    var topp=0;
+    var x = 0;
+    var y = 0;
 
+
+    var masu = new Object();
+        masu.yoko = [25,74,123,172,221];
+        masu.tate = [25,78,131,184];
+        masu.menn =[
+            [1,2,3,4,5],
+            [6,7,8,9,10],
+            [11,12,13,14,15],
+            [16,17,18,19,20]
+        ];
+    
     function pcheck(){
         left = $('#koma').position().left;
         topp = $('#koma').position().top;
@@ -32,21 +45,45 @@ $(function(){
             }
     }
 
+    function torappu(){
+        if(x % 2 == 1){
+            susumu();
+            console.log("１マス進みます。");
+        }else{
+            modoru();
+            console.log("１マス戻ります。");
+        };
+    }
+
     $('#saik').click(function(){               //サイコロを振るをクリックしたとき
         saiko = Math.floor(Math.random()*6)+1;
-        console.log(saiko);
-        cnt = cnt + saiko;
-        console.log(cnt);
+        console.log("サイコロの目は"+saiko);
         pcheck();
 
         for(var i = saiko;i>0;i--){
-            console.log(left);
             susumu();
             
             if (topp >= 184 && left >= 221){
                 document.write("ゴール");
                 break;
             }
+        }
+
+        pcheck();
+        x=0;
+        y=0;
+        while(masu.yoko[x] != left){
+            x++;
+        };
+        while(masu.tate[y] != topp){
+            y++;
+        };
+        console.log("現在地"+masu.menn[y][x]);
+        x = masu.menn[y][x];
+        torappu();
+
+        if (topp >= 184 && left >= 221){
+            document.write("ゴール");
         }
     })
 });
