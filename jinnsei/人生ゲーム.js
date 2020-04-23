494b199877d31e09e917fbbd84f1ffb606c35b02
Takkun3 <56062548+Takkun3@users.noreply.github.com>
2020-04-23 20:27:34 +0900

    関数追加

--- jinnsei/人生ゲーム.js
+++ jinnsei/人生ゲーム.js
@@ -10,30 +10,19 @@ $(function(){
         topp = $('#koma').position().top;
     }
 
-    function modoru(){
-
-    }
-
-    function susumu(){
-
-    }
-
-    $('#saik').click(function(){
-        saiko = Math.floor(Math.random()*6)+1;
-        console.log(saiko);
-        cnt = cnt + saiko;
-        console.log(cnt);
-        pcheck();
-
-        for(var i = saiko;i>0;i--){
-
-            if (topp >= 184 && left >= 221){
-                document.write("ゴール");
-                break;
+    function modoru(){                       //１マス戻る
+        left = left - 49;
+            $('#koma').css("left",left);
+            if (left <= 25){
+                topp = topp -53;
+                left = 221;
+                $('#koma').css("left",left);
+                $('#koma').css("top",topp);                
             }
-            
-            console.log(left);
-            left = left + 49;
+    }
+
+    function susumu(){　　　　　　　　　　　　　//１マス進む
+        left = left + 49;
             $('#koma').css("left",left);
             if (left >= 270){
                 topp = topp +53;
@@ -41,6 +30,19 @@ $(function(){
                 $('#koma').css("left",left);
                 $('#koma').css("top",topp);                
             }
+    }
+
+    $('#saik').click(function(){               //サイコロを振るをクリックしたとき
+        saiko = Math.floor(Math.random()*6)+1;
+        console.log(saiko);
+        cnt = cnt + saiko;
+        console.log(cnt);
+        pcheck();
+
+        for(var i = saiko;i>0;i--){
+            console.log(left);
+            susumu();
+            
             if (topp >= 184 && left >= 221){
                 document.write("ゴール");
                 break;
